09a52a3e5cb74525ac4aa1d3af2aff521a74073e
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-07-19 11:03:15 -0500

    fetch first 10 character names from API

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const[names,setNames] = useState([]);
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+    .get(`https://swapi.co/api/people/`)
+    .then(response => {
+      console.log('Data pulled: ',response);
+      const charArray = response.data.results;
+      console.log('Character array: ',charArray);
+      const namesArray =[];
+      charArray.forEach(character => {
+        console.log("Character: ",character);
+        namesArray.push(character.name);
+      })
+      console.log(namesArray);
+      setNames(namesArray);
+    })
+    .catch(error => {
+      console.log('The API is currently down, try again later', error)
+    })
+  },[])
 
   return (
     <div className="App">
