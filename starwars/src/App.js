3c010f2e28016401362d466de2b1643d390b7b52
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-07-19 12:03:34 -0500

    added stock image for all characters

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -7,6 +7,7 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const[names,setNames] = useState([]);
+  const[planets, setPlanets] = useState([]);
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -19,12 +20,14 @@ const App = () => {
       const charArray = response.data.results;
       console.log('Character array: ',charArray);
       const namesArray =[];
+      const planetsArray =[];
       charArray.forEach(character => {
         console.log("Character: ",character);
         namesArray.push(character.name);
+        planetsArray.push(character.homeworld);
       })
-      console.log(namesArray);
       setNames(namesArray);
+      setPlanets(planetsArray);
     })
     .catch(error => {
       console.log('The API is currently down, try again later', error)
@@ -34,9 +37,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <div>{
-        names.map(name => {
-        return <CharCard key = {name} name = {name} />
+      <div>
+        {names.map(name => {
+        return <CharCard key={name} name={name} homeworld= {planets[0]}/>
       })}
       </div>
     </div>
--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function CharCard({name}) {
+export default function CharCard({name,homeworld}) {
     return (
         <div className = "char-card">
+            <img src="https://freedesignfile.com/upload/2017/08/astronaut-icon-vector.png"/>
             <h2>{name}</h2>
+            <a href={homeworld}>{homeworld}</a>
         </div>
     );
 }
